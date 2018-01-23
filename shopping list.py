12993aa05be76bcc64ea269c70d9ee1961415af3
Anija <anija.nair@yahoo.com>
2018-01-23 13:53:42 -0500

    added 'shopping list.py'

--- shopping list.py
+++ shopping list.py
@@ -1,6 +1,22 @@
 # make a list
+shopping_list = []
+
 # give instructions to user
+print("What is the item to be added to the shopping list?")
+print("Enter 'DONE' to finish the list")
+
 # ask for item to be added
-# add item to the list
-# be able to quit
+while True:
+    new_item = input("> ")
+
+    # be able to quit
+    if new_item == 'DONE':
+        break
+
+    # add item to the list
+    shopping_list.append(new_item)
+
 # print the list
+print("Here's the shopping list: ")
+for item in shopping_list:
+    print(item)
