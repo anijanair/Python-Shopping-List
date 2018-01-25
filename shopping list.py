1a6918f4ecba54726309aa7996e39832999f3fe7
Anija <anija.nair@yahoo.com>
2018-01-25 07:37:04 -0500

    added 'HELP', 'SHOW' commands to shopping list

--- shopping list.py
+++ shopping list.py
@@ -1,9 +1,37 @@
 # make a list
+# have a 'HELP' command
+# have a 'SHOW' command
+
 shopping_list = []
 
 # give instructions to user
-print("What is the item to be added to the shopping list?")
-print("Enter 'DONE' to finish the list")
+
+print("What is the item to be added to the shopping list? ")
+
+
+def show_help():
+    print("""
+Enter 'DONE' to finish the list
+Enter 'HELP' for this help
+Enter 'SHOW' to see the current list
+""")
+
+
+show_help()
+
+
+# print the list
+def show_list():
+    print("Here's the shopping list: ")
+    for item in shopping_list:
+        print(item)
+
+
+# add item to the list
+def add_an_item():
+    shopping_list.append(new_item)
+    print("Added {}. List now has {} items".format(new_item, len(shopping_list)))
+
 
 # ask for item to be added
 while True:
@@ -12,11 +40,18 @@ while True:
     # be able to quit
     if new_item == 'DONE':
         break
+    elif new_item == 'HELP':
+        show_help()
+        continue
+    elif new_item == 'SHOW':
+        show_list()
+        continue
+    else:
+        add_an_item()
 
-    # add item to the list
-    shopping_list.append(new_item)
+show_list()
 
-# print the list
-print("Here's the shopping list: ")
-for item in shopping_list:
-    print(item)
+
+
+
+
